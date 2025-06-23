6fb02497aaba2af467cdfe1a246809cf3fb5ead6
Minghu <chumi379@outlook.com>
2025-06-23 16:47:51 +0800

    feat: readbook，搭建微服务项目骨架，Maven多模块方式。

---
+++ redbook-auth/src/main/java/top/chumi/redbook/auth/RedbookAuthApplication.java
@@ -0,0 +1,13 @@
+package top.chumi.redbook.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbookAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedbookAuthApplication.class, args);
+    }
+
+}
---
+++ redbook-auth/src/test/java/top/chumi/redboo/auth/RedbookAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chumi.redbook.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbookAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
